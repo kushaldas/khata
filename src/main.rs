0c1b5f48b1be4fe3beada847daac2a1a81a4167e
Kushal Das <mail@kushaldas.in>
2019-08-09 22:55:47 +0530

    Adds command line arguments
    Uses clap library to have command line arguments to the tool.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 extern crate chrono;
+extern crate clap;
 extern crate khata;
 
+use clap::{crate_authors, crate_version, App, Arg};
 use khata::utils::*;
 
 fn main() {
-    create_new_post();
+    let matches = App::new("khata")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Static blogging tool inspired from Shonku.")
+        .arg(
+            Arg::with_name("new")
+                .short("n")
+                .long("new")
+                .help("Creates a new blog post under posts directory.")
+                .takes_value(false),
+        )
+        .get_matches();
+    if matches.is_present("new") {
+        create_new_post();
+        return;
+    }
 }
